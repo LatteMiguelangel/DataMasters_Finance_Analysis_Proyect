0eaa964485cc1d9209566e35a2bf8500798653ab
LatteMiguelangel <leonet.miguelangel.2021@gmail.com>
2024-11-17 14:47:38 -0400

    data 100x100 limpia

--- scripts/data_manipulation.py
+++ scripts/data_manipulation.py
@@ -24,15 +24,15 @@ apple_df['Date'] = pd.to_datetime(apple_df['Date'])
 won_to_usd_00_17_df['DATE'] = pd.to_datetime(won_to_usd_00_17_df['DATE'])
 won_to_usd_04_22_df['Date'] = pd.to_datetime(won_to_usd_04_22_df['Date'])
 
-samsung_df['Date'] = samsung_df['Date'].dt.strftime('%d-%m-%Y')
-apple_df['Date'] = apple_df['Date'].dt.strftime('%d-%m-%Y')
-won_to_usd_00_17_df['DATE'] = won_to_usd_00_17_df['DATE'].dt.strftime('%d-%m-%Y')
-won_to_usd_04_22_df['Date'] = won_to_usd_04_22_df['Date'].dt.strftime('%d-%m-%Y')
+samsung_df['Date'] = samsung_df['Date'].dt.strftime('%Y/%m/%d')
+apple_df['Date'] = apple_df['Date'].dt.strftime('%Y/%m/%d')
+won_to_usd_00_17_df['DATE'] = won_to_usd_00_17_df['DATE'].dt.strftime('%Y/%m/%d')
+won_to_usd_04_22_df['Date'] = won_to_usd_04_22_df['Date'].dt.strftime('%Y/%m/%d')
 
 
 #renombrado de columnas
-samsung_df.rename(columns={'Date': 'date', 'Open': 'samsung_open', 'High': 'samsung_high', 'Low': 'samsung_low', 'Close': 'samsung_close', 'Adj Close': 'samsung_adj_close', 'Volume': 'samsung_volume'}, inplace=True)
-apple_df.rename(columns={'Date': 'date', 'Open': 'apple_open', 'High': 'apple_high', 'Low': 'apple_low', 'Close': 'apple_close', 'Adj Close': 'apple_adj_close', 'Volume': 'apple_volume'}, inplace=True)
+samsung_df.rename(columns={'Date': 'date', 'Open': 'open', 'High': 'high', 'Low': 'low', 'Close': 'close', 'Adj Close': 'adj_close', 'Volume': 'volume'}, inplace=True)
+apple_df.rename(columns={'Date': 'date', 'Open': 'open', 'High': 'high', 'Low': 'low', 'Close': 'close', 'Adj Close': 'adj_close', 'Volume': 'volume'}, inplace=True)
 won_to_usd_00_17_df.rename(columns={'DATE': 'date', 'DEXKOUS': 'dollar'}, inplace=True)
 won_to_usd_04_22_df.rename(columns={'Date': 'date', 'KRW=X': 'dollar'}, inplace=True)
 
@@ -55,5 +55,36 @@ won_to_usd_04_22_df.rename(columns={'Date': 'date', 'KRW=X': 'dollar'}, inplace=
 #combinar datasets de valores de conversion entre dolar y won coreano
 combined_dollar_values = pd.concat([won_to_usd_00_17_df, won_to_usd_04_22_df])
 combined_dollar_values = combined_dollar_values.drop_duplicates(subset='date')
-#print(combined_dollar_values)
-st.dataframe(combined_dollar_values)
+
+
+#agregar columna de conversion de won a dolar
+samsung_df['usd_to_won'] = samsung_df['date'].map(combined_dollar_values.set_index('date')['dollar'])
+
+
+#filtrar filas con volumen 0 y eliminar filas con valores nulos
+samsung_df = samsung_df[samsung_df['volume'] != 0]
+samsung_df = samsung_df.dropna()
+
+
+#convertir valores de won a dolar en la tabla de samsung
+samsung_df['open'] = samsung_df.apply(lambda x: float(x['open']) / float(x['usd_to_won']), axis=1)
+samsung_df['high'] = samsung_df.apply(lambda x: float(x['high']) / float(x['usd_to_won']), axis=1)
+samsung_df['low'] = samsung_df.apply(lambda x: float(x['low']) / float(x['usd_to_won']), axis=1)
+samsung_df['close'] = samsung_df.apply(lambda x: float(x['close']) / float(x['usd_to_won']), axis=1)
+samsung_df['adj_close'] = samsung_df.apply(lambda x: float(x['adj_close']) / float(x['usd_to_won']), axis=1)
+
+# crear columna de year ---> samsung_df['year'] = pd.DatetimeIndex(samsung_df['date']).year
+
+#rangos para evaluar acciones de apple
+date_start = samsung_df['date'].min()
+date_end = samsung_df['date'].max()
+#definicion final de acciones de apple
+apple_df = apple_df[(apple_df['date'] >= date_start) & (apple_df['date'] <= date_end)]
+
+
+#mostrar df
+st.dataframe(samsung_df.sort_values('date', ascending=True))
+st.divider()
+st.dataframe(apple_df.sort_values('date', ascending=True))
+
+
