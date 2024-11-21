b3d51b854c138c9bcce25cc451613998bc792f79
LatteMiguelangel <leonet.miguelangel.2021@gmail.com>
2024-11-21 00:05:44 -0400

    update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -29,10 +29,9 @@ samsung_df = samsung_df.dropna()
 apple_df = apple_df.dropna()
 won_to_usd_00_17_df = won_to_usd_00_17_df.dropna()
 won_to_usd_04_22_df = won_to_usd_04_22_df.dropna()
-
+#borrar filas con valores '.'
 won_to_usd_00_17_df = won_to_usd_00_17_df[won_to_usd_00_17_df['DEXKOUS'] != '.']
 won_to_usd_04_22_df = won_to_usd_04_22_df[won_to_usd_04_22_df['KRW=X'] != '.']
-
 
 #formateando las columnas de fechas
 samsung_df['Date'] = pd.to_datetime(samsung_df['Date'])
@@ -45,42 +44,22 @@ apple_df['Date'] = apple_df['Date'].dt.strftime('%Y/%m/%d')
 won_to_usd_00_17_df['DATE'] = won_to_usd_00_17_df['DATE'].dt.strftime('%Y/%m/%d')
 won_to_usd_04_22_df['Date'] = won_to_usd_04_22_df['Date'].dt.strftime('%Y/%m/%d')
 
-
 #renombrado de columnas
 samsung_df.rename(columns={'Date': 'date', 'Open': 'open', 'High': 'high', 'Low': 'low', 'Close': 'close', 'Adj Close': 'adj_close', 'Volume': 'volume'}, inplace=True)
 apple_df.rename(columns={'Date': 'date', 'Open': 'open', 'High': 'high', 'Low': 'low', 'Close': 'close', 'Adj Close': 'adj_close', 'Volume': 'volume'}, inplace=True)
 won_to_usd_00_17_df.rename(columns={'DATE': 'date', 'DEXKOUS': 'dollar'}, inplace=True)
 won_to_usd_04_22_df.rename(columns={'Date': 'date', 'KRW=X': 'dollar'}, inplace=True)
 
-# ----- definición de índice --------
-# samsung_df.set_index('date', inplace=True)
-# apple_df.set_index('date', inplace=True)
-# won_to_usd_00_17_df.set_index('date', inplace=True)
-# won_to_usd_04_22_df.set_index('date', inplace=True)
-
-
-#print(samsung_df.head())
-#print('\n\n\n-----------------------------------------\n\n')
-#print(apple_df.head())
-
-#print(won_to_usd_04_22_df.sort_values('dollar'))
-#print('\n\n\n-----------------------------------------\n\n')
-#print(won_to_usd_00_17_df.sort_values('dollar'))
-
-
 #combinar datasets de valores de conversion entre dolar y won coreano
 combined_dollar_values = pd.concat([won_to_usd_00_17_df, won_to_usd_04_22_df])
 combined_dollar_values = combined_dollar_values.drop_duplicates(subset='date')
 
-
 #agregar columna de conversion de won a dolar
 samsung_df['usd_to_won'] = samsung_df['date'].map(combined_dollar_values.set_index('date')['dollar'])
-
 
 #filtrar filas con volumen 0 y eliminar filas con valores nulos
 samsung_df = samsung_df[samsung_df['volume'] != 0]
 samsung_df = samsung_df.dropna()
-
 
 #convertir valores de won a dolar en la tabla de samsung
 samsung_df['open'] = samsung_df.apply(lambda x: float(x['open']) / float(x['usd_to_won']), axis=1)
@@ -99,7 +78,6 @@ date_end = samsung_df['date'].max()
 #definicion final de acciones de apple
 apple_df = apple_df[(apple_df['date'] >= date_start) & (apple_df['date'] <= date_end)]
 
-
 #mostrar df
 st.title("Análisis Financiero: Samsung vs Apple")
 st.write("""
@@ -113,13 +91,3 @@ st.write("""
 Esta tabla contiene las acciones de **Apple** convertidas a dólares del **2000 al 2022**.
 """)
 st.dataframe(apple_df.sort_values('date', ascending=True).reset_index(drop=True, inplace=False))
-
-
-
-
-# Gráfico interactivo
-# company = st.selectbox("Selecciona una empresa", ["Samsung", "Apple"])
-# if company == "Samsung":
-#     st.line_chart(samsung_df[['year', 'volume']].set_index('year'))
-# else:
-#     st.line_chart(apple_df[['year', 'volume']].set_index('year'))
