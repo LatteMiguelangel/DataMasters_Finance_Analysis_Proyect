cab4bc8f4710328dadc22ae8491f7a42299aada2
LatteMiguelangel <leonet.miguelangel.2021@gmail.com>
2024-11-22 19:42:51 -0400

    app.py update

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import streamlit as st
+import matplotlib.colors as mcolors
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import seaborn as sns
 
 st.set_page_config(
-    page_title="Samsung vs Apple",
+    page_title="Data Analysis",
     page_icon="📊",
     layout="centered"
 )
@@ -16,9 +20,26 @@ hide_streamlit_style = """
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
+colors = {
+    'Sony': '#4285F4', 
+    'IBM': '#6c3483', 
+    'Google': '#EA4335', 
+    'Microsoft': '#F14F21', 
+    'Amazon': '#d68910', 
+    'Nvidia': '#34A853', 
+    'Samsung': '#1428A0',
+    'Apple': '#B5B5B5'
+}
+
 #lectura de datasets
 samsung_df = pd.read_csv('data/raw/SSNG.csv', delimiter=',')
 apple_df = pd.read_csv('data/raw/AAPL.csv', delimiter=',')
+amazon_df = pd.read_csv('data/raw/AMZN.csv', delimiter=',')
+microsoft_df = pd.read_csv('data/raw/MSFT.csv', delimiter=',')
+nvidia_df = pd.read_csv('data/raw/NVDA.csv', delimiter=',')
+google_df = pd.read_csv('data/raw/GOOG.csv', delimiter=',')
+sony_df = pd.read_csv('data/raw/SONY.csv', delimiter=',')
+ibm_df = pd.read_csv('data/raw/IBM.csv', delimiter=',')
 won_to_usd_00_17_df = pd.read_csv('data/raw/KRW_TO_USD_2000-2017.csv', delimiter=',')
 won_to_usd_04_22_df = pd.read_csv('data/raw/KRW_TO_USD_2004-2022.csv', delimiter=',')
 won_to_usd_04_22_df = won_to_usd_04_22_df[['Date', 'KRW=X']]
@@ -26,6 +47,12 @@ won_to_usd_04_22_df = won_to_usd_04_22_df[['Date', 'KRW=X']]
 #eliminar filas con valores nulos
 samsung_df = samsung_df.dropna()
 apple_df = apple_df.dropna()
+amazon_df = amazon_df.dropna()
+microsoft_df = microsoft_df.dropna()
+nvidia_df = nvidia_df.dropna()
+google_df = google_df.dropna()
+sony_df = sony_df.dropna()
+ibm_df = ibm_df.dropna()
 won_to_usd_00_17_df = won_to_usd_00_17_df.dropna()
 won_to_usd_04_22_df = won_to_usd_04_22_df.dropna()
 
@@ -35,6 +62,12 @@ won_to_usd_04_22_df = won_to_usd_04_22_df[won_to_usd_04_22_df['KRW=X'] != '.']
 #formateando las columnas de fechas
 samsung_df['Date'] = pd.to_datetime(samsung_df['Date'])
 apple_df['Date'] = pd.to_datetime(apple_df['Date'])
+amazon_df['Date'] = pd.to_datetime(amazon_df['Date'])
+microsoft_df['Date'] = pd.to_datetime(microsoft_df['Date'], format='%m/%d/%Y')
+nvidia_df['Date'] = pd.to_datetime(nvidia_df['Date'])
+google_df['Date'] = pd.to_datetime(google_df['Date'])
+sony_df['Date'] = pd.to_datetime(sony_df['Date'])
+ibm_df['Date'] = pd.to_datetime(ibm_df['Date'])
 won_to_usd_00_17_df['DATE'] = pd.to_datetime(won_to_usd_00_17_df['DATE'])
 won_to_usd_04_22_df['Date'] = pd.to_datetime(won_to_usd_04_22_df['Date'])
 
@@ -46,6 +79,12 @@ won_to_usd_04_22_df['Date'] = won_to_usd_04_22_df['Date'].dt.strftime('%Y/%m/%d'
 #renombrado de columnas
 samsung_df.rename(columns={'Date': 'date', 'Open': 'open', 'High': 'high', 'Low': 'low', 'Close': 'close', 'Adj Close': 'adj_close', 'Volume': 'volume'}, inplace=True)
 apple_df.rename(columns={'Date': 'date', 'Open': 'open', 'High': 'high', 'Low': 'low', 'Close': 'close', 'Adj Close': 'adj_close', 'Volume': 'volume'}, inplace=True)
+amazon_df.rename(columns={'Date': 'date', 'Open': 'open', 'High': 'high', 'Low': 'low', 'Close': 'close', 'Adj Close': 'adj_close', 'Volume': 'volume'}, inplace=True)
+microsoft_df.rename(columns={'Date': 'date', 'Open': 'open', 'High': 'high', 'Low': 'low', 'Close': 'close', 'Adj Close': 'adj_close', 'Volume': 'volume'}, inplace=True)
+nvidia_df.rename(columns={'Date': 'date', 'Open': 'open', 'High': 'high', 'Low': 'low', 'Close': 'close', 'Adj Close': 'adj_close', 'Volume': 'volume'}, inplace=True)
+google_df.rename(columns={'Date': 'date', 'Open': 'open', 'High': 'high', 'Low': 'low', 'Close': 'close', 'Adj Close': 'adj_close', 'Volume': 'volume'}, inplace=True)
+sony_df.rename(columns={'Date': 'date', 'Open': 'open', 'High': 'high', 'Low': 'low', 'Close': 'close', 'Adj Close': 'adj_close', 'Volume': 'volume'}, inplace=True)
+ibm_df.rename(columns={'Date': 'date', 'Open': 'open', 'High': 'high', 'Low': 'low', 'Close': 'close', 'Adj Close': 'adj_close', 'Volume': 'volume'}, inplace=True)
 won_to_usd_00_17_df.rename(columns={'DATE': 'date', 'DEXKOUS': 'dollar'}, inplace=True)
 won_to_usd_04_22_df.rename(columns={'Date': 'date', 'KRW=X': 'dollar'}, inplace=True)
 
@@ -67,26 +106,88 @@ samsung_df['low'] = samsung_df.apply(lambda x: float(x['low']) / float(x['usd_to
 samsung_df['close'] = samsung_df.apply(lambda x: float(x['close']) / float(x['usd_to_won']), axis=1)
 samsung_df['adj_close'] = samsung_df.apply(lambda x: float(x['adj_close']) / float(x['usd_to_won']), axis=1)
 
-# crear columna de year para ambos datasets
-samsung_df['year'] = pd.DatetimeIndex(samsung_df['date']).year
-apple_df['year'] = pd.DatetimeIndex(apple_df['date']).year
 
-#rangos para evaluar acciones de apple
-date_start = samsung_df['date'].min()
-date_end = samsung_df['date'].max()
-#definicion final de acciones de apple
-apple_df = apple_df[(apple_df['date'] >= date_start) & (apple_df['date'] <= date_end)]
+#2022-05-23 -> 23 de Mayo del 2022
+#2000-02-01 -> 2 de Febrero del 2000
+START = samsung_df['date'].min() 
+END = samsung_df['date'].max() 
+apple_df = apple_df[(apple_df['date'] >= START) & (apple_df['date'] <= END)]
+amazon_df = amazon_df[(amazon_df['date'] >= START) & (amazon_df['date'] <= END)]
+microsoft_df = microsoft_df[(microsoft_df['date'] >= START) & (microsoft_df['date'] <= END)]
+nvidia_df = nvidia_df[(nvidia_df['date'] >= START) & (nvidia_df['date'] <= END)]
+google_df = google_df[(google_df['date'] >= START) & (google_df['date'] <= END)]
+sony_df = sony_df[(sony_df['date'] >= START) & (sony_df['date'] <= END)]
+ibm_df = ibm_df[(ibm_df['date'] >= START) & (ibm_df['date'] <= END)]
+
+#Reseteo de indices
+samsung_df.reset_index(drop=True, inplace=True)
+apple_df.reset_index(drop=True, inplace=True)
+amazon_df.reset_index(drop=True, inplace=True)
+microsoft_df.reset_index(drop=True, inplace=True)
+nvidia_df.reset_index(drop=True, inplace=True)
+google_df.reset_index(drop=True, inplace=True)
+sony_df.reset_index(drop=True, inplace=True)
+ibm_df.reset_index(drop=True, inplace=True)
 
 #mostrar df
-st.title("Análisis Financiero: Samsung vs Apple")
+st.title("Comportamiento de las Acciones de las Empresas Big Tech")
 st.write("""
 ### Samsung Dataset
 Esta tabla contiene las acciones de **Samsung** convertidas a dólares del **2000 al 2022**.
 """)
-st.dataframe(samsung_df.sort_values('date', ascending=True).reset_index(drop=True, inplace=False))
+st.dataframe(samsung_df.sort_values('date', ascending=True))
 st.divider()
 st.write("""
 ### Apple Dataset
 Esta tabla contiene las acciones de **Apple** convertidas a dólares del **2000 al 2022**.
 """)
-st.dataframe(apple_df.sort_values('date', ascending=True).reset_index(drop=True, inplace=False))
+st.dataframe(apple_df.sort_values('date', ascending=True))
+
+st.header("""
+Modulo de Estadísticas Descriptivas
+""")
+st.write("""
+        #### Distribución de Datos
+""")
+datasets = {'Samsung': samsung_df, 'Apple': apple_df, 'Amazon': amazon_df, 'Microsotf':microsoft_df, 'Nvidia': nvidia_df, 'Google': google_df, 'Sony': sony_df, 'IBM': ibm_df}
+for company, data in datasets.items():
+    # Excluir la columna 'date' y calcular las estadísticas descriptivas
+    st.write(f"Estadísticas para {company}:")
+    st.write(data.drop(columns=['date']).describe())
+
+st.divider()
+
+st.write("""
+        #### Análisis de Volatilidad
+""")
+
+for company, data in datasets.items():
+    data['daily_range'] = data['high'] - data['low']
+    data['daily_pct_change'] = (data['close'] - data['open']) / data['open'] * 100
+    st.write(f"Resumen de volatilidad para {company}:\n")
+    st.write(data[['daily_range', 'daily_pct_change']].describe())
+    
+st.write("""
+        #### Análisis de Correlación de Variables por Compañía
+""")
+for company, data in datasets.items():
+    # Filtrar solo las columnas numéricas
+    numeric_data = data.select_dtypes(include='number')
+    
+    if numeric_data.empty:
+        st.subheader(f"{company}: No hay datos numéricos disponibles para calcular la correlación.")
+        continue
+
+    # Calcular la matriz de correlación
+    correlation_matrix = numeric_data.corr()
+
+    # Crear el heatmap con Seaborn
+    fig, ax = plt.subplots(figsize=(8, 6))  # Tamaño ajustable para cada gráfico
+    sns.set_style('whitegrid')
+    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', ax=ax)
+    ax.set_title(f'Correlación de variables para {company}')
+
+    # Mostrar el gráfico en Streamlit
+    st.subheader(f"Matriz de Correlación: {company}")
+    st.pyplot(fig)
+    
